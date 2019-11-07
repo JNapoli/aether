d9f248c6ea8c059f5b80ac45a1467df9ec2f1a05
jnapoli <jnapoli@stanford.edu>
2019-11-07 14:49:37 -0800

    Make inference more robust by transforming to grayscale

--- bin/app.py
+++ bin/app.py
@@ -53,6 +53,7 @@ def transform_image(image_bytes):
     """
 
     my_transforms = transforms.Compose([
+        transforms.Grayscale(num_output_channels=1),
         transforms.Resize(28),
         transforms.ToTensor(),
         transforms.Normalize([0.5,], [0.5,])
